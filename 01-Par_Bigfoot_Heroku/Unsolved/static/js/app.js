6f1dd5f1869cccabd1ffe3eaa4f64eec573d9645
ymikawi <45125566+ymikawi@users.noreply.github.com>
2019-03-16 10:41:42 -0400

    Add files via upload

---
+++ 01-Par_Bigfoot_Heroku/Unsolved/static/js/app.js
@@ -0,0 +1,33 @@
+/* data route */
+var url = "/data";
+
+function buildPlot() {
+  d3.json(url).then(function(response) {
+
+    console.log(response);
+    var trace = {
+      type: "scatter",
+      mode: "lines",
+      name: "Bigfoot Sightings",
+      x: response.map(data => data.year),
+      y: response.map(data => data.sightings)
+    };
+
+    var data = [trace];
+
+    var layout = {
+      title: "Bigfoot Sightings Per Year",
+      xaxis: {
+        type: "date"
+      },
+      yaxis: {
+        autorange: true,
+        type: "linear"
+      }
+    };
+
+    Plotly.newPlot("plot", data, layout);
+  });
+}
+
+buildPlot();
